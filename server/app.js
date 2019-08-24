291fff17fc288949fdaa9caf035b5ec1f40d675b
Basil <mbonubasil@gmail.com>
2019-08-24 17:19:36 +0100

    Setup server for heroku deployment

--- server/app.js
+++ server/app.js
@@ -3,9 +3,10 @@ const config = require("./config");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const contactRoutes = require("./routes/contact");
+const path = require("path");
 
 const app = express();
-const PORT = config.PORT;
+const PORT = config.PORT || 5000;
 
 //mongo setup
 mongoose
@@ -17,5 +18,13 @@ mongoose
 app.use(express.json());
 app.use(cors());
 app.use("/api/contacts", contactRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
