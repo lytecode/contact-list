548a6ce04cc8dff0f936803714dd0a5d973ae9d3
Basil <mbonubasil@gmail.com>
2019-08-24 03:06:57 +0100

    Initial client commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppNavbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
